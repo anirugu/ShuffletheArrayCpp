31ebb1b9377c7dbee96969ab3d7f9b9a028f4a51
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-01-26 15:21:07 +0530

    Initial commit

---
+++ ShuffletheArrayCpp.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+       swap(nums)
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> vals{ 2,5,1,3,4,7 };
+    sol.shuffle(vals,3);
+    std::cout << "Hello World!\n";
+}
+/*
+251
+347
+*/
